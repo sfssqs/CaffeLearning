f979fc57f168f5e534d9ad846508dc292eab13f8
Xing Xia <xiaxingwork@gmail.com>
2017-11-30 22:43:51 +0800

    empty Xcode project

---
+++ CaffeLearning/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CaffeLearning
+//
+//  Created by xiaxing on 17/11/30.
+//  Copyright (c) 2017年 shyla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
